573ae05e5300d7ba925e1950c45af0431f376b6a
sn1ket1985 <otto_lundberg@hotmail.com>
2025-05-07 16:46:38 +0200

    B - progress i Asteroirds

--- main.py
+++ main.py
@@ -7,25 +7,39 @@
 
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+    
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+    
+  
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("Window close clicked")
                 return
-            
-            elif event.type == pygame.KEYDOWN:
-                print(f"Key pressed: {pygame.key.name(event.key)}")
-
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                print(f"Mouse clicked at {event.pos}")
+        
+        updatable.update(dt)
+        
         screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+             
         pygame.display.flip()
         
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
     
